58d0e7d06bf2859d81207cd7bb66d8837a952383
hammel <hammel@debian.catv.wtnet.de>
2012-06-25 22:19:42 +0200

    buttons sind da

--- src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/barzahlung/BarZahlungsWerkzeugUI.java
+++ src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/barzahlung/BarZahlungsWerkzeugUI.java
@@ -1,11 +1,13 @@
 package de.uni_hamburg.informatik.swt.se2.kino.werkzeuge.barzahlung;
 
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.sql.Date;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 /**
@@ -17,9 +19,14 @@ import javax.swing.JTextField;
  */
 public class BarZahlungsWerkzeugUI{
 
+	//dialog
 	private JDialog _dialog;
+	//buttons & co
+	private JPanel _buttonPanel;
 	private JButton _ok;
 	private JButton _abbrechen;
+	
+	//todo
 	private JTextField _preisEingabe;
 	private JLabel _preisGesamt;
 	private JLabel _preisRest;
@@ -37,20 +44,32 @@ public class BarZahlungsWerkzeugUI{
 		assert gesammtBetrag > 0 : "Vorbedingung verletzt bzgl. gesamtbetrag";
 		
 		//TODO layout
+		//Anderes Layout?
 		
 		System.out.printf("Running UI Constructor");
 		
+		//Dialog
 		_dialog = new JDialog();
-		_ok = new JButton("OK");
-		_abbrechen = new JButton("Abbrechen");
-		_preisEingabe = new JTextField();
-		_preisGesamt = new JLabel();
-		_preisRest = new JLabel();
-
+		_dialog.setLayout( new BorderLayout());
 		_dialog.setSize(300, 200);//?
 		_dialog.setTitle("Barzahlung");
 		
-		_dialog.setLayout(new BorderLayout());
+		//Buttons & Co.
+		_buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+		_ok = new JButton("OK");
+		_abbrechen = new JButton("Abbrechen");
+		_dialog.add(_buttonPanel, BorderLayout.SOUTH);
+		_buttonPanel.add( _ok, BorderLayout.SOUTH);
+		_buttonPanel.add( _abbrechen, BorderLayout.SOUTH);
+
+		
+		//TODO
+		
+		/*_preisEingabe = new JTextField("0");
+		_preisGesamt = new JLabel();
+		_preisRest = new JLabel();*/
+
+
 		
 		
 		_dialog.setModal(true);
