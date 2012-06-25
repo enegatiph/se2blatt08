4a3447579627dbe2336d66afe242228aa26efd8d
enegatiph <enegatiph@googlemail.com>
2012-06-25 22:17:44 +0200

    BarZahlungsWerkzeug.istBarZahlungMoeglich(int)

--- src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/barzahlung/BarZahlungsWerkzeug.java
+++ src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/barzahlung/BarZahlungsWerkzeug.java
@@ -14,15 +14,35 @@ public class BarZahlungsWerkzeug  extends BeobachtbaresSubWerkzeug{
  	BarZahlungsWerkzeugUI _ui;
 	int _gesammtBetrag;
 	
-	public BarZahlungsWerkzeug(int gesammtBetrag) {
+	public BarZahlungsWerkzeug(int gesammtBetrag) 
+	{
 		_gesammtBetrag = gesammtBetrag;
 		_ui = new BarZahlungsWerkzeugUI(gesammtBetrag);
 	}
 	
+	/**
+	 * 
+	 * @param betragVomKunden
+	 *        Der Kunde muss mind. den gesamtBetrag Bezahlen
+	 * @return
+	 *        true: wenn der Kunde zahlen kann
+	 *        false: andererseits
+	 */
+	public boolean istBarZahlungMoeglich(int betragVomKunden)
+	{
+		if(betragVomKunden >= _gesammtBetrag)
+		{	
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
 	
 	/* methods
-	    BarZahlungsWerkzeug( int gesamtBetrag)
-		istBarZahlungMoeglich( int betragVomKunden) : boolean
+	    
+		
 		registriereUIAktionen(): void
 		reagiereAufAbbrechenButton() : void
 		reagiereAufOkButton() : void
