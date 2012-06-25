df4779ebe67f119f0610391ce41e7d1916bcbfb6
hammel <hammel@debian.catv.wtnet.de>
2012-06-25 21:36:40 +0200

    dummy constructor BarZahlWerk ( + UI ); test konstructor for BZW in PVW added

--- src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/barzahlung/BarZahlungsWerkzeug.java
+++ src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/barzahlung/BarZahlungsWerkzeug.java
@@ -4,14 +4,13 @@ import de.uni_hamburg.informatik.swt.se2.kino.werkzeuge.BeobachtbaresSubWerkzeug
 
 public class BarZahlungsWerkzeug  extends BeobachtbaresSubWerkzeug{
 
-	public BarZahlungsWerkzeug() {
-		// TODO Auto-generated constructor stub
+ 	BarZahlungsWerkzeugUI _ui;
+	int _preis;
+	
+	public BarZahlungsWerkzeug(int preis) {
+		_preis = preis;
+		_ui = new BarZahlungsWerkzeugUI();
 	}
-
-	/* atr
-	 	_ui: BarZahlungsWerkzeug
-		_preis : int
-	 */
 	
 	
 	/* methods
